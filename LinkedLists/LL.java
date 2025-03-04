47adaacc026cbe4481e9174202c4fa7a4bae7c67
Krish Sharma <krishsharma9005@gmail.com>
2025-03-04 23:53:13 +0530

    Linked List

---
+++ LinkedLists/LL.java
@@ -0,0 +1,174 @@
+package LinkedLists;
+
+public class LL {
+    public static class Node {
+        int data;
+        Node next;
+        public Node(int data) { // can pass Node next as well, but if we not pass it will automatically take null
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node convertArr2LL(int arr[]) {
+        Node head = new Node(arr[0]);
+        Node mover = head;
+        for(int i = 1; i<arr.length; i++) {
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp; // mover = mover.next;
+        }
+        return head;
+    } //O(N)
+    public static int sizeOfLL(Node head) {
+        int cnt = 0;
+        Node temp = head;
+        while(temp != null) {
+            temp = temp.next;
+            cnt++;
+        }
+        return cnt;
+    }
+    public static void printLL(Node head) {
+        Node temp = head;
+        while(temp != null) {
+            System.out.print(temp.data +" ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    public static boolean searchLL(Node head, int key) {
+        Node temp = head;
+        while(temp != null) {
+            if(temp.data == key) return true;
+            temp = temp.next;
+        }
+        return false;
+    }
+    public static Node deleteHead(Node head) {
+        if(head == null) return head;
+        head = head.next;
+        return head;
+    }
+    public static Node deleteTail(Node head) {
+        if(head == null || head.next == null) return null;
+        Node temp = head;
+        while(temp.next.next != null) {
+            temp = temp.next;
+        }
+        temp.next = null;
+        return head;
+    }
+    public static Node deleteNode(Node head, int k) {
+        if(head == null) return head;
+        if(k==1) {
+            head = head.next;
+            return head;
+        }
+        Node temp = head;
+        Node prev = null;
+        int i = 1;
+        while(temp != null) {
+            if(i == k) {
+                prev.next = prev.next.next;
+                break;
+            }
+            prev = temp;
+            temp = temp.next;
+            i++;
+        }
+        return head;
+    }
+    public static Node deleteEle(Node head, int el) {
+        if(head == null) return head;
+        if(head.data == el) {
+            head = head.next;
+            return head;
+        }
+        Node temp = head;
+        Node prev = null;
+        while(temp != null) {
+            if(temp.data == el) {
+                prev.next = prev.next.next;
+                break;
+            }
+            prev = temp;
+            temp = temp.next;
+        }
+        return head;
+    }
+    public static Node addHead(Node head) {
+        Node newNode = new Node(8);
+        if(head == null) {
+            head = newNode;
+            return head;
+        }
+        newNode.next = head;
+        head = newNode;
+        return head;
+    }
+    public static Node addTail(Node head) {
+        Node newNode = new Node(5);
+        if(head == null) {
+            head = newNode;
+            return head;
+        }
+        Node temp = head;
+        while(temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        newNode.next = null;
+        return head;
+    }
+    public static Node addNode(Node head, int k) {
+        Node newNode = new Node(9);
+        if(head == null) {
+            head = newNode;
+            return head;
+        }
+        if(k==1) {
+            newNode.next = head;
+            return newNode;
+        }
+        Node temp = head;
+        Node prev = null;
+        int i = 1;
+        while(temp != null) {
+            if(i == k) {
+                prev.next = newNode;
+                newNode.next = temp;
+                break;
+            }
+            prev = temp;
+            temp = temp.next;
+            i++;
+        }
+        return head;
+    }
+    public static void main(String[] args) {
+       int arr[] = {1,2,3,4};
+       Node head = convertArr2LL(arr);
+       //System.out.println(head.data);
+
+       printLL(head);
+
+       //System.out.println("size: "+ sizeOfLL(head));
+
+       //System.out.println(searchLL(head, 20));
+
+       //head = deleteHead(head);
+       //printLL(head);
+
+       //deleteTail(head);
+       //printLL(head);
+
+       //head = deleteNode(head, 2);
+       //printLL(head);
+
+       //head = deleteEle(head, 2);
+       //printLL(head);
+
+       head = addNode(head, 3);
+       printLL(head);
+    }
+}
---
+++ LinkedLists/LinkedList.java
@@ -0,0 +1,274 @@
+package LinkedLists;
+
+public class LinkedList {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) { //Constructor
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data) {
+        //step1 -> Create new Node
+        Node newNode = new Node(data);
+        size++;
+        if(head == null) { //if there is no node
+            head = tail = newNode;
+            return;
+        }
+        //step2 -> newNode next = head
+        newNode.next = head; //link
+
+        //step3 -> head = newNode
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if(head == null) { //empty linkedlist
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void printLL() {
+        if(head == null) {
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp = head;
+        while(temp != null) {
+            System.out.print(temp.data +"->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void addMiddle(int idx, int data) {
+        if(idx == 0) {
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        int i = 0;
+        while(i < idx-1) {
+            temp = temp.next;
+            i++;
+        }
+
+        //i = idx-1; temp -> prev
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    public int removeFirst() {
+        if(size == 0) {
+            System.out.println("LL is empty");
+            return Integer.MAX_VALUE;
+        }
+        else if(size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        int val = head.data; 
+        head = head.next; //the second node now becomes the head
+        size--;
+        return val;
+    }
+
+    public int removeLast() {
+        if(size == 0) {
+            System.out.println("LL is empty");
+            return Integer.MAX_VALUE;
+        }
+        else if(size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        //prev node: idx = size-2
+        Node prev = head;
+        for(int i = 0; i<size-2; i++) { //loop to get prev
+            prev = prev.next;
+        }
+        int val = tail.data;
+        prev.next = null;
+        prev = tail;
+        size--;
+        return val;
+    }
+
+    public int linearSearch(int key) { //TC-> O(N)
+        Node temp = head;
+        int index = 0;
+        while(temp!=null) {
+            if(temp.data == key) {
+                return index;
+            }
+            temp = temp.next;
+            index++;
+        }
+        return -1;
+    }
+
+    public int helper(Node head, int key) { //helper fxn for recursive search
+        if(head == null) {
+            return -1;
+        }
+        if(head.data == key) {
+            return 0;
+        }
+        int idx = helper(head.next, key);
+        if(idx == -1) {
+            return -1;
+        }
+        return idx+1;
+    } //TC-> O(N), SC-> O(N) {bcz of call stack}
+    public int recursiveSearch(int key) {
+        return helper(head, key);
+    }
+    
+    public void reverse() {
+        Node prev = null;
+        Node curr = head;
+        Node next;
+        while(curr!=null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    } //TC-> O(N), SC-> O(1)
+
+    public void deleteNthfromEnd(int n) {
+        if(head == null) {
+            return;
+        }
+        // Calculate size
+        int size = 0;
+        Node temp = head;
+        while(temp!=null) {
+            temp = temp.next;
+            size++;
+        }
+
+        if(n == size) { //Corner Case
+            head = head.next; //removeFirst
+            return;
+        }
+
+        //to reach (size-n)th idx {prev}
+        int i = 1;
+        int idxToFind = size-n; //finding prev node 
+        Node prev = head;
+        while(i < idxToFind) {
+            prev = prev.next;
+            i++;
+        }
+        prev.next = prev.next.next; //prev ke next ka next
+        return;
+    } //TC-> O(L) {L is the no of nodes}, SC-> O(1) 
+
+    //Slow-Fast Concept
+    public Node findMid(Node head) { //helper
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next; //+1
+            fast = fast.next.next; //+2
+        }
+        return slow; //slow is my midNode
+    }
+
+    public boolean isPalindrome() {
+        if(head == null || head.next == null) { //empty LL or single element
+            return true;
+        }
+        //step1 - find mid
+        Node midNode = findMid(head);
+
+        //step2 - reverse 2nd half
+        Node prev = null;
+        Node curr = midNode;
+        Node next;
+        while(curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        Node right = prev; //right half ka head
+        Node left = head;
+
+        //step3 - check left half & right half
+        while(right != null) {
+            if(left.data != right.data) {
+                return false;
+            }
+            //updation
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+    } //TC-> O(N), SC->O(1)
+
+    public static void main(String args[]) {
+        LinkedList ll = new LinkedList();
+        // ll.addFirst(2);
+        // ll.addFirst(1);
+        // TC -> O(1) - while adding elements 
+        // ll.addLast(4);
+        // ll.addLast(5);
+        // TC -> O(1) - while adding Last Elements
+
+        // ll.addMiddle(2, 3);
+
+        // ll.printLL();
+        //System.out.println(ll.size);
+
+        // ll.removeFirst();
+        // ll.printLL();
+
+        // ll.removeLast();
+        // System.out.println(ll.removeLast());
+        // ll.printLL();
+        // System.out.println(ll.size);
+
+        //System.out.println(ll.linearSearch(3));
+
+        // System.out.println(ll.recursiveSearch(5));
+
+        // ll.reverse();
+        // ll.printLL();
+
+        // ll.deleteNthfromEnd(3);
+        // ll.printLL();
+
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(1);
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(1);
+        ll.printLL();
+        System.out.println(ll.isPalindrome());
+    } 
+}
