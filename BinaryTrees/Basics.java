4337f64663bcef9a6c1e06969f000799987759e6
Krish Sharma <krishsharma9005@gmail.com>
2025-06-01 09:54:53 +0530

    Binary Trees

--- BinaryTrees/Basics.java
+++ BinaryTrees/Basics.java
@@ -188,6 +188,8 @@ public class Basics {
             }
             System.out.println();
         }
+
+        
     }
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
